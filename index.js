f9c3769455725622075e69bf331c88b50963d3ef
steven0129 <h3584935@gmail.com>
2023-03-04 10:12:24 +0800

    Open Feedly website when starting the application
    Signed-off-by: steven0129 <h3584935@gmail.com>

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    zoomToPageWidth: true
   });
 
-  win.loadFile('index.html');
+  win.loadURL('https://feedly.com')
+  win.maximize()
 };
 
 app.whenReady().then(() => {
